9de051ba78af44036e0d7f9a935e497fb043f4d6
gastonstuart-lab <gastonstuart@googlemail.com>
2026-06-18 09:57:39 +0800

    test: isolate Firestore emulator suites from unit tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    exclude: ['**/node_modules/**', '**/dist/**', '**/firestore.rules.test.ts'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/firestore.rules.test.ts', '**/firestore-rules/**'],
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
   },
