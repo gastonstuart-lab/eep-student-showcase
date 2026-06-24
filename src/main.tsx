1f22f10401793cdd90f3e74cc0e9781a8a94337a
gastonstuart-lab <223871820+gastonstuart-lab@users.noreply.github.com>
2026-06-24 10:15:20 +0800

    Make intro the first root experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,23 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { IedEntryPage } from './components/public/IedEntryPage'
 import './admin-layout-fix.css'
 import './workspace.css'
 
+function RootExperience() {
+  const [entered, setEntered] = useState(window.location.pathname !== '/')
+
+  if (!entered) {
+    return <IedEntryPage onComplete={() => setEntered(true)} />
+  }
+
+  return <App />
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RootExperience />
   </StrictMode>,
 )
